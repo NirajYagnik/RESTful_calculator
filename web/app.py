752d9342e12339d13984d168f88ea0b27c3a130e
NirajYagnik <nirajyagnik@gmail.com>
2020-04-10 00:43:03 +0530

    added docker and mongodb support

--- web/app.py
+++ web/app.py
@@ -1,8 +1,24 @@
 from flask import Flask,jsonify, request
 from flask_restful import Api, Resource
 
+from pymongo import MongoClient
+
 app = Flask(__name__)
 api = Api(app)
+
+client =  MongoClient("mongodb://db:27017")
+db = client.aNewDB
+UserNum = db["UserNum"]
+
+UserNum.insert({
+  'num_of_user':0
+})
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_user']
+        new_num = prev_num + 1
+        UserNum.update({}, {"$set":{"num_of_user":new_num}})
+        return str("User "+ str(new_num))
 
 def checkPostedData(postedData, functionName):
     if(functionName == "add"or functionName == "subtract" or functionName == "multiply"):
@@ -123,6 +139,7 @@ api.add_resource(Add,"/add")
 api.add_resource(Subtract,"/subtract")
 api.add_resource(Multiply,"/multiply")
 api.add_resource(Divide,"/divide")
+api.add_resource(Visit,"/hello")
 
 
 
@@ -132,4 +149,4 @@ def hello_world():
     return "Hello World"
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host ='0.0.0.0')
